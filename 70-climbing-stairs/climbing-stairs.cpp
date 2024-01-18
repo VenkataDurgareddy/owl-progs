f183dc405fd072033438d79828fba38c246e7035
Satti Venkata Durga Reddy <97235269+VenkataDurgareddy@users.noreply.github.com>
2024-01-18 22:27:53 +0530

    Time: 2 ms (45.07%) | Memory: 6.4 MB (71.72%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=3)
+        {
+            return n;
+        }
+        int a=1;
+        int b=2;
+        int c=a+b;
+        for(int i=3;i<=n;i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+    }
+};
