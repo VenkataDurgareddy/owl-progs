8aefa0443a5ca9a02f4f0a7d01be91de85a4112b
Satti Venkata Durga Reddy <97235269+VenkataDurgareddy@users.noreply.github.com>
2024-02-01 18:16:49 +0530

    A frog jumps

--- Dynamic-programming/A frog jumps.cpp
+++ Dynamic-programming/A frog jumps.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
- int dp[10001];
+ int dp[100001];
 int frogjump(int ind,vector<int>&v,int n)
 {
     if(ind==n-1)
@@ -10,7 +10,7 @@ int frogjump(int ind,vector<int>&v,int n)
     if(dp[ind]!=-1) return dp[ind];
     int left=abs(v[ind]-v[ind+1])+frogjump(ind+1,v,n);
     int right=INT_MAX;
-    if(ind+2<=n-1)
+    if((ind+2)<=n-1)
     {
         right=abs(v[ind]-v[ind+2])+frogjump(ind+2,v,n);
     }
@@ -18,10 +18,16 @@ int frogjump(int ind,vector<int>&v,int n)
 }
 int main()
 {
-    //int n;
-    // cin>>n;
-    vector<int>v={10,30,40,20};
+    int n;
+    cin>>n;
+    vector<int>v;
+    for(int i=0;i<n;i++)
+    {
+        int k;
+        cin>>k;
+        v.push_back(k);
+    }
     memset(dp,-1,sizeof(dp));
-    int ans=frogjump(0,v,4);
+    int ans=frogjump(0,v,n);
     cout<<ans;
 }
