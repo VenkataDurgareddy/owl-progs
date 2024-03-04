eda516e986895c9aab94d2fedae1e43e27141656
Satti Venkata Durga Reddy <97235269+VenkataDurgareddy@users.noreply.github.com>
2024-03-04 18:27:23 +0530

    Time: 3 ms (86.63%) | Memory: 13 MB (70.94%) - LeetSync

--- 985-bag-of-tokens/bag-of-tokens.cpp
+++ 985-bag-of-tokens/bag-of-tokens.cpp
@@ -7,14 +7,14 @@ public:
         int j=n-1;
         int maxscore=INT_MIN;
         int i=0;
-        if(n==1)
-        {
-            if(power>=tokens[0])
-            {
-                return 1;
-            }
-        }
-        while(i<n and j>0)
+        // if(n==1)
+        // {
+        //     if(power>=tokens[0])
+        //     {
+        //         return 1;
+        //     }
+        // }
+        while(i<n and j>=0)
         {
             if(power>=tokens[i]  )
             {
