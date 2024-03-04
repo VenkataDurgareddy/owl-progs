c3ef8dc26e864d4d670a99de9b36c62614cc4eca
Satti Venkata Durga Reddy <97235269+VenkataDurgareddy@users.noreply.github.com>
2024-03-04 18:26:06 +0530

    Time: 6 ms (40.29%) | Memory: 12.9 MB (70.94%) - LeetSync

--- 985-bag-of-tokens/bag-of-tokens.cpp
+++ 985-bag-of-tokens/bag-of-tokens.cpp
@@ -4,7 +4,6 @@ public:
         int score=0;
         sort(tokens.begin(),tokens.end());
         int n=tokens.size();
-        vector<int>vis(n,0);
         int j=n-1;
         int maxscore=INT_MIN;
         int i=0;
@@ -17,19 +16,17 @@ public:
         }
         while(i<n and j>0)
         {
-            if(power>=tokens[i] and vis[i]==0 )
+            if(power>=tokens[i]  )
             {
                 power=power-tokens[i];
                 score++;
-                vis[i]=1;
                 i++;
             }
-            else if(score>=1 and vis[j]==0)
+            else if(score>=1)
             {
                 power=power+tokens[j];
                 maxscore=max(maxscore,score);
                 score--;
-                vis[j]=1;
                 j--;
             }
            else{
