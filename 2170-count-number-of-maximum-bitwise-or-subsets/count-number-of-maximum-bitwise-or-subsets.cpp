bfe5542a28f411d7ce42f2590d727739cb87485a
Satti Venkata Durga Reddy <97235269+VenkataDurgareddy@users.noreply.github.com>
2024-03-10 22:20:23 +0530

    Time: 12 ms (81.59%) | Memory: 9.7 MB (79.85%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    int ans;
-    map<int,int>m;
-    int subset(vector<int>&nums, int i, int n, int bitore, vector<int>&v) {
-        if (i >= n) {
-            bitore = 0;
-            for (auto it : v) {
-                bitore = bitore | it;
-            }
-            m[bitore]++;
-            ans = max(ans, bitore);
-            return ans;
+    int maxOR;
+    int countMaxOR;
+    
+    void dfs(vector<int>& nums, int index, int currentOR) {
+        if (index == nums.size()) {
+            if (currentOR == maxOR)
+                countMaxOR++;
+            return;
         }
-        v.push_back(nums[i]);
-        subset(nums, i + 1, n, bitore, v);
-        v.pop_back();
-        subset(nums, i + 1, n, bitore, v);
-        return ans;
+        dfs(nums, index + 1, currentOR | nums[index]);
+        dfs(nums, index + 1, currentOR);
     }
+    
     int countMaxOrSubsets(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> v;
-        int an = subset(nums, 0, n, 0, v);
-        return m[an];
+        maxOR = 0;
+        for (int num : nums)
+            maxOR |= num;
+        countMaxOR = 0;
+        dfs(nums, 0, 0);
+        return countMaxOR;
     }
 };
