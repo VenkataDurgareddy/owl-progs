41ae171341d1edf95899a53dc892203c27f1c21a
Satti Venkata Durga Reddy <97235269+VenkataDurgareddy@users.noreply.github.com>
2024-04-24 19:36:07 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (81.41%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,21 +1,16 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-    vector<int> dp;
-
-    Solution() {
-        dp.resize(38, -1);
+    int dp[38];
+    int tri(int n)
+    {
+        if(dp[n]!=-1)return dp[n];
+        if(n==0)return 0;
+        if(n==1)return 1;
+        if(n==2)return 1;
+        return dp[n]=tri(n-1)+tri(n-2)+tri(n-3);
     }
-
     int tribonacci(int n) {
-        if (n <= 0) {
-            return 0;
-        } else if (n == 1 || n == 2) {
-            return 1;
-        }
-        if (dp[n] != -1) return dp[n];
-        return dp[n] = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+        memset(dp,-1,sizeof(dp));
+       return tri(n);
     }
 };
