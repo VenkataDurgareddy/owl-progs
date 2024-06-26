49fd8ff8691da271578d54b2e55aa9fbe0429ff7
Satti Venkata Durga Reddy <97235269+VenkataDurgareddy@users.noreply.github.com>
2024-06-26 10:28:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of occurrence/number-of-occurrence.cpp
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	/* if x is present in arr[] then returns the count
+		of occurrences of x, otherwise returns 0. */
+		int lower(int arr[],int n,int x)
+		{
+		    int low=0,high=n-1;
+		    int ans=-1;
+		    while(low<=high)
+		    {
+		        int mid=(low+high)/2;
+		        if(arr[mid]==x)
+		        {
+		            ans=mid;
+		            high=mid-1;
+		        }
+		        else if(arr[mid]<x)
+		        {
+		            low=mid+1;
+		        }
+		        else{
+		            high=mid-1;
+		        }
+		    }
+		    return ans;
+		}
+		int higher(int arr[],int n,int x)
+		{
+		    int low=0,high=n-1;
+		    int ans=-1;
+		    while(low<=high)
+		    {
+		        int mid=(low+high)/2;
+		        if(arr[mid]==x)
+		        {
+		           ans=mid;
+		           low=mid+1;
+		        }
+		        else if(arr[mid]<x)
+		        {
+		            low=mid+1;
+		        }
+		        else{
+		            high=mid-1;
+		        }
+		    }
+		    return ans;
+		}
+	int count(int arr[], int n, int x) {
+	    // code here
+	    int lowerbound=lower(arr,n,x);
+	    int higherbound=higher(arr,n,x);
+	    if(higherbound==-1 || lowerbound==-1)return 0;
+	    int ans=(higherbound-lowerbound)+1;
+	    return ans;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.count(arr, n, x);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
