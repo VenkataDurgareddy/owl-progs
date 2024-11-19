6582b56b7d876245c9cd0f38b85fc7ec7360af49
Satti Venkata Durga Reddy <97235269+VenkataDurgareddy@users.noreply.github.com>
2024-11-19 23:15:38 +0530

    Time: 97 ms (35.82%) | Memory: 75.6 MB (17.19%) - LeetSync

---
+++ 2954-maximum-sum-of-almost-unique-subarray/maximum-sum-of-almost-unique-subarray.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    long long maxSum(vector<int>& nums, int m1, int k) {
+        int i=0,j=0;
+        long long sum=0,ans=0;
+        map<int,int>m;
+        while(j<nums.size())
+        {
+            sum+=nums[j];
+            m[nums[j]]++;
+            if((j-i)+1>k)
+            {
+               sum-=nums[i];
+                m[nums[i]]--;
+                if(m[nums[i]]==0)
+                {
+                    m.erase(nums[i]);
+                }
+                i++; 
+            }
+            while(m.size()<m1 && ((j-i)+1)==k)
+            {
+                sum-=nums[i];
+                m[nums[i]]--;
+                if(m[nums[i]]==0)
+                {
+                    m.erase(nums[i]);
+                }
+                i++;
+            }
+            if((j-i)+1==k)
+            {
+                ans=max(ans,sum);
+            }
+            j++;
+        }
+        return ans;
+    }
+};
